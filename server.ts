aaf8e059159952ec1c7aadb64d69a13851bf179b
Jose Neto <josevieira2029@gmail.com>
2023-11-25 01:11:51 -0300

    Organizando arquitetura

--- server.ts
+++ server.ts
@@ -3,28 +3,25 @@ import { retornaPrestadorExistente } from "./middlewares";
 import { criarAnuncio, deletaAnuncio, editaAnuncio, listaAnuncioPrestador, listaTodosAnuncios } from "./controller/anuncioController/anuncioController";
 import express from 'express';
 
-
-const anuncioRoutes = express();
-anuncioRoutes.use(express.json())
+const app = express();
+app.use(express.json())
 
 
 // cria anuncio associado a um prestador 
-anuncioRoutes.post('/anuncio', retornaPrestadorExistente, criarAnuncio);
+app.post('/anuncio', retornaPrestadorExistente, criarAnuncio);
 
 // lista os anuncios associados a um prestador
-anuncioRoutes.get('/anunciosPrestador', retornaPrestadorExistente, listaAnuncioPrestador);
+app.get('/anunciosPrestador', retornaPrestadorExistente, listaAnuncioPrestador);
 
 // lista todos os anuncios cadastrados
-anuncioRoutes.get('/anuncios', listaTodosAnuncios);
+app.get('/anuncios', listaTodosAnuncios);
 
 // edita um anuncio 
-anuncioRoutes.put('/anuncios/:id', editaAnuncio);
+app.put('/anuncios/:id', editaAnuncio);
 
 // deleta um anuncio
-anuncioRoutes.delete('/anuncio/:id', deletaAnuncio);
+app.delete('/anuncio/:id', deletaAnuncio);
 
-anuncioRoutes.listen(3005, () => {
+app.listen(3005, () => {
     console.log("conectado");
 });
-
-export { anuncioRoutes }
