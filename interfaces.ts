ca5eaf84c5a8a71b448785d8be2cf740d13a7ede
joyce <joyce069.js@gmail.com>
2023-11-25 01:01:39 -0300

    Criando middleware de autenticação de prestador de serviço

---
+++ interfaces.ts
@@ -0,0 +1,13 @@
+export interface payload{
+    id: string
+}
+
+export interface prestador{
+    id: string;
+    nome: string;
+    email: string;
+    senha: string;
+    telefone: string;
+    endereco: string;
+    foto: string;
+}
